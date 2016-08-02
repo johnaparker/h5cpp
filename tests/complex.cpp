a3dd6b168dace258ff4e192ce8784ad0c63cf755
John <japarker@uchicago.edu>
2016-08-02 18:48:35 -0500

    working example of compound datatype

--- tests/complex.cpp
+++ tests/complex.cpp
@@ -16,6 +16,44 @@ struct my_complex {
     double imag;
 };
 
+struct crazy {
+    crazy(): one(0), two(0) {}
+    crazy(double r, int i, string s): one(r), two(i), three(s.c_str()) {}
+    double one;
+    int two;
+    const char* three;
+};
+
+class compound_dtype {
+public:
+    compound_dtype(size_t memsize) {
+        memtype = H5Tcreate (H5T_COMPOUND, memsize);
+        memDisplace = 0;
+    }
+
+    void insert(string name, dtype datatype) {
+        hid_t h5dtype = getDtype(datatype);
+        status = H5Tinsert (memtype, name.c_str(),
+                    memDisplace, h5dtype);
+
+        if (datatype == dtype::String)
+            memDisplace += sizeof(hvl_t);
+        else
+            memDisplace += H5Tget_size(h5dtype);
+
+    }
+
+    hid_t getType() {
+        return memtype;
+    }
+
+private:
+    hid_t memtype;
+    herr_t status;
+    size_t memDisplace;
+};
+
+
 int main() {
     //boost array properties
     
@@ -42,17 +80,33 @@ int main() {
     dataset.write(values.data());
 
 
-    hid_t memtype;
-    herr_t status;
+    // create complex datatype
+    //compound_dtype complexType (sizeof(my_complex));
+    //complexType.insert("real", dtype::Double);
+    //complexType.insert("imag", dtype::Double);
 
-    memtype = H5Tcreate (H5T_COMPOUND, sizeof (my_complex));
-    status = H5Tinsert (memtype, "real",
-                HOFFSET (my_complex, real), H5T_NATIVE_DOUBLE);
-    status = H5Tinsert (memtype, "imag", HOFFSET (my_complex, imag),
-                H5T_NATIVE_DOUBLE);
+    compound_dtype complexType (sizeof(complex<double>));
+    complexType.insert("real", dtype::Double);
+    complexType.insert("imag", dtype::Double);
+
     dims = {n};
-    auto my_dataset = f.create_dataset("my_data", memtype, dspace(dims));
+    auto my_dataset = f.create_dataset("my_data", complexType.getType(), dspace(dims));
     my_dataset.write(my_values.data());
+
+
+    compound_dtype crazyType (sizeof(crazy));
+    crazyType.insert("one" , dtype::Double);
+    crazyType.insert("two" , dtype::Int);
+    crazyType.insert("three" , dtype::String);
+
+    crazy out[2];
+    out[0] = crazy(1.3, 0, "hi");
+    out[1] = crazy(1.3, -1, "bye");
+
+    dims = {2};
+    auto crazy_dataset = f.create_dataset("crazy", crazyType.getType(), dspace(dims));
+    crazy_dataset.write(&(out[0]));
+
 }
 
 
