f7550498dcace24aa493200a51bf10cb521a80d5
John <japarker@uchicago.edu>
2017-06-11 20:43:55 -0500

    template specialization on eigen write functions so that attributes can be easily writen to datasets

--- include/eigen.h
+++ include/eigen.h
@@ -20,6 +20,11 @@
         dset.write(m.data()); \
         return dset;
 
+#define ATTR_TEMPLATE_CODE auto dspace = get_eigen_dspace(dims, a); \
+        auto attr = dset.create_attribute(name, type_map[typeid(T)], dspace); \
+        attr.write(m.data()); \
+        return attr;
+
 namespace h5cpp {
 
     extern std::unordered_map<std::type_index, dtype> type_map;
@@ -75,6 +80,36 @@ namespace h5cpp {
         auto dset = group.create_dataset(name, type_map[typeid(T)], ds);
         dset.write(&scalar);
         return dset;
+    }
+
+    //specialization of write_scalar for attribute in a dataset
+    template<class T>
+    h5cpp::h5attr write_scalar(const T &scalar, const h5dset &dset, std::string name, append a = append::False) {
+        auto ds = bool(a) ? get_eigen_dspace({1}, a) : dspace();
+        auto attr = dset.create_attribute(name, type_map[typeid(T)], ds);
+        attr.write(&scalar);
+        return attr;
+    }
+
+    //specialization of write_vecotr for attribute in a dataset
+    template<class T>
+    h5cpp::h5attr write_vector(const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> &m, const h5dset &dset, std::string name, append a = append::False) {
+        auto dims = std::vector<hsize_t>{m.size()};
+        ATTR_TEMPLATE_CODE
+    }
+
+    //specialization of write_array for attribute in a dataset
+    template<class T>
+    h5cpp::h5attr write_array(const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> &m, const h5dset &dset, std::string name, append a = append::False) {
+        auto dims = std::vector<hsize_t>{m.size()};
+        ATTR_TEMPLATE_CODE
+    }
+
+    //specialization of write_matrix for attribute in a dataset
+    template<class T>
+    h5cpp::h5attr write_matrix(const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> &m, const h5dset &dset, std::string name, append a = append::False) {
+        auto dims = std::vector<hsize_t>{m.rows(), m.cols()};
+        ATTR_TEMPLATE_CODE
     }
 
     // append functions (append_vector, append_matrix, etc.) (create or open)
